7f95a266903e46429b0c0cc45eb7dfbbdaac9d65
Adriel Perkins <adriel@adrielperkins.dev>
2023-11-11 14:20:39 -0500

    feat: bump version of schemacheck to 1.7.8

--- Formula/schemacheck.rb
+++ Formula/schemacheck.rb
@@ -2,8 +2,8 @@ class Schemacheck < Formula
   desc "CLI utility to validate yaml and json files against a schema written in Go"
   homepage "https://github.com/adrielp/schemacheck"
   url "https://github.com/adrielp/schemacheck.git",
-    tag:      "v1.7.4",
-    revision: "08bb475ee3369aec9bade39e9ec8beca99242c55"
+    tag:      "v1.7.8",
+    revision: "23567fc8145454e9dda58170301ebbfadc7e54d1"
   license "MIT"
   head "https://github.com/adrielp/schemacheck.git", branch: "main"
 
